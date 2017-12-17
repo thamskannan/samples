3ae7a21873e9b3b0dd11e182144970b093e82bdb
Kannan Dharmalingam <kannan.dharmalingam@globallogic.com>
2017-12-16 19:15:59 -0800

    Adding exception message

--- com/kannan/collection/thread/DeadLock.java
+++ com/kannan/collection/thread/DeadLock.java
@@ -24,7 +24,9 @@ public class DeadLock {
                 System.out.println("Thread1 : holding lock 1");
                 try {
                     Thread.sleep(10);
-                } catch (InterruptedException e) {}
+                } catch (InterruptedException e) {
+                    System.out.println("Thread1: Interrupted");
+                }
                 System.out.println("Thread1: waiting for lock 2");
                 synchronized (lock2) {
                     System.out.println("Thread1: holding lock 1,2");
@@ -39,7 +41,9 @@ public class DeadLock {
                 System.out.println("Thread2 : holding lock 2");
                 try {
                     Thread.sleep(10);
-                } catch (InterruptedException e) {}
+                } catch (InterruptedException e) {
+                    System.out.println("Thread2: Interrupted");
+                }
                 System.out.println("Thread2: waiting for lock 1");
                 synchronized (lock1) {
                     System.out.println("Thread2: holding lock 1,2");
